46a50958c861252ecd456914852d6883a8e016ec
OverAcheiever <keshava.reddy.100580@standrewsindia.com>
2021-02-09 01:15:46 +0530

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ let fieldData = {};
 let amount = 0;
 // Grab Field Data onWidgetLoad
 window.addEventListener("onWidgetLoad", function (obj) {
-  const fieldData = obj.detail.fieldData;
+  fieldData = obj.detail.fieldData;
 });
 
 // Initialise WebSocket Connection to Server
@@ -13,40 +13,38 @@ const socket = new WebSocket(
 
 // Reconnect on Close
 socket.addEventListener("close", () => {
-  console.log("Reconnecting");
-  setInterval(function () {
+  console.log("Reconnecting to Server");
+ setInterval(function () {
     new WebSocket(
-      "wss://rally-alerts-server.herokuapp.com/"
+      "wss://rally-alerts-server.herokuapp.com/rally/alerts/websocket"
     );
-  }, 1000);
+  }, 5000);
+});
+
+socket.addEventListener('open', (event) => {
+  setInterval(function(){socket.send('Hello Server!');},20000)
 });
 
 // Listen to Data
 socket.addEventListener("message", (event) => {
+  socket.send('Received!')
   if (event.data.slice(0, 1) !== "{") {
     console.log("SERVER: ", event.data);
   } else {
     console.log("Donation Receieved");
-    if (event.coinKind == fieldData.creatorCoin) {
-      Alert(
-        event.data.fromUsername,
-        parseInt(JSON.parse(event.data).amountOfCoin)
-      );
+    if (JSON.parse(event.data).coinKind == String(fieldData.creatorCoin)) {
+      console.log(event.data)
+      Alert(event);
     }
   }
 });
 
 //Alerts
-function Alert(name, amount) {
+function Alert(event) {
   console.log("ALERTS: Playing Alert");
-  function upto2Decimal(num) {
-    if (num > 0) return Math.floor(num * 100) / 100;
-    else return Math.ceil(num * 100) / 100;
-  }
-  amount = upto2Decimal(amount);
-  $(".username").html(name);
-
-  $(".amount").html(`$${amount}`);
+  
+  $(".username").html(JSON.parse(event.data).data.fromUsername);
+  $(".amount").html(JSON.parse(event.data).data.amountOfCoin.toFixed(2));
 
   gsap
     .timeline()
@@ -63,7 +61,7 @@ function Alert(name, amount) {
       .fromTo(".username", { y: 0, opacity: 1 }, { y: -10, opacity: 0 })
       .fromTo(".message", { y: 0, opacity: 1 }, { y: -10, opacity: 0 });
   }, 3000);
-
+	setTimeout(resumeQueue,6000)
   function resumeQueue() {
     SE_API.resumeQueue();
   }
@@ -71,7 +69,28 @@ function Alert(name, amount) {
 
 window.addEventListener("onEventReceived", function (obj) {
   if ("widget-button" === obj.detail.event.listener) {
-    Alert("OverAchiever", "50");
+    $(".username").html("OverAchiever");
+  $(".amount").html(50);
+
+  gsap
+    .timeline()
+    .fromTo(".img", { opacity: 0 }, { opacity: 1 })
+    .fromTo(".username", { y: -10, opacity: 0 }, { y: 0, opacity: 1 })
+    .fromTo(".message", { y: -10, opacity: 0 }, { y: 0, opacity: 1 });
+
+  setTimeout(function () {
+    gsap
+      .timeline({
+        onComplete: resumeQueue,
+      })
+      .fromTo(".img", { opacity: 1 }, { opacity: 0 })
+      .fromTo(".username", { y: 0, opacity: 1 }, { y: -10, opacity: 0 })
+      .fromTo(".message", { y: 0, opacity: 1 }, { y: -10, opacity: 0 });
+  }, 3000);
+	setTimeout(resumeQueue,6000)
+  function resumeQueue() {
+    SE_API.resumeQueue();
+  }
   } else {
     SE_API.resumeQueue();
   }
